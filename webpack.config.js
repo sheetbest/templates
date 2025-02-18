a79e88127cf4cfbf9cf204c53892110d23fe97c2
Alfredo Matos <alfredo@sidenotehq.com>
2025-02-18 17:22:53 +0000

    build improvements

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,40 @@
 const path = require('path');
+const fs = require('fs');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const pagesDir = path.resolve(__dirname, 'src/pages');
+const entry = {
+  index: path.resolve(__dirname, 'src/index.js')
+};
+
+fs.readdirSync(pagesDir).forEach(folder => {
+  const folderPath = path.join(pagesDir, folder);
+  if (fs.statSync(folderPath).isDirectory()) {
+    entry[folder] = path.join(folderPath, 'index.js');
+  }
+});
+
+const htmlPlugins = [
+  new HtmlWebpackPlugin({
+    filename: 'index.html',
+    template: path.resolve(__dirname, 'src/index.html'),
+    chunks: ['index'],
+  }),
+  ...Object.keys(entry).filter(name => name !== 'index').map(name => {
+    return new HtmlWebpackPlugin({
+      filename: `${name}.html`,
+      template: path.join(pagesDir, name, 'index.html'),
+      chunks: [name],
+    });
+  })
+];
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry,
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: '[name].js',
   },
   module: {
     rules: [
@@ -24,12 +52,16 @@ module.exports = {
     ],
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
+    ...htmlPlugins
   ],
   devServer: {
     static: './dist',
     open: true,
+    historyApiFallback: {
+      rewrites: Object.keys(entry).map(name => ({
+        from: new RegExp(`^/${name}$`),
+        to: `/${name}.html`
+      })),
+    },
   },
 };
